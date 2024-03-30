ceef704d8dc6d22ce6b2dcfb5e52cbaf4b322ab9
ThomasNoto <tnoto82@gmail.com>
2024-03-30 11:04:05 -0400

    Added test copy csv

--- backend/src/main/java/com/example/backend/shoppingListInteractor.java
+++ backend/src/main/java/com/example/backend/shoppingListInteractor.java
@@ -12,8 +12,37 @@ public class shoppingListInteractor {
         BufferedWriter bw = new BufferedWriter(fw);
         bw.write(item);
     }
+
+    private List<List<String>> makeList(){
+        List<List<String>> data = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader fileName)) {
+            String line;
+
+            while ((line = avgReader.readLine()) != null) {
+                List<String> row = Arrays.asList(line.split(","));
+                data.add(row);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+
     public void remove(String item){
-        
+        List<List<String>> data = makeList();
+        Iterator<List<String>> outerIterator = data.iterator();
+        while (outerIterator.hasNext()) {
+            List<String> innerList = outerIterator.next();
+            if (innerList.contains(item)) {
+                // Remove item flagged for delteion from
+                // inner list if found
+                outerIterator.remove();
+            }
+        }
 
     }
+
+
+
 }
